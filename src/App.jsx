7f66b62f69916f3854ade09b9cd409a9da75a86a
Prateeksingchn <prateeksinghchouhan4499@gmail.com>
2024-05-20 02:16:45 +0530

    2nd commit for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <section>
+      <section className='dark:bg-[#101010] w-[100vw] mx-auto min-h-[100vh]'>
 
-{/* ------------------- GRID-1  --------------------- */}
-          <div className='grid grid-cols-1 gap-4 list-none lg:grid-cols-3 lg:grid-rows-3 xl:grid-cols-4 xl:grid-rows-4'>
+        <div className='p-4 '>
+          <div className='grid grid-cols-1 gap-4 list-none lg:grid-cols-3 lg:grid-rows-3 xl:grid-cols-4 xl:grid-rows-4 '>
+
+
+          {/* ------------------- GRID-1  --------------------- */}
             <div className='lg:row-span-2 ring-1 dark:ring-white/10 ring-primary/5 bg-[#1A1A1A] dark:bg-secondary shadow-xl dark:shadow-thick rounded-3xl p-8'>
               <div className='relative flex items-center gap-x-4'>
                   <img
@@ -39,9 +41,8 @@ function App() {
               Outside of the screen, I enjoy playing football or working out at the gym. My life is a balanced mix of technology and nature, switching between focused coding sessions and moments of creative inspiration.
               </p>
             </div>
-          </div>
 
-{/* ------------------- GRID-2 (Find me on section starts here )  --------------------- */}
+          {/* ------------------- GRID-2 (Find me on section starts here )  --------------------- */}
           <div className='ring-1 dark:ring-white/10 ring-primary/5 rounded-3xl justify-between shadow-xl dark:shadow-thick items-center flex flex-col p-8 h-full bg-[#1A1A1A]  dark:bg-secondary'>
             <div className='w-full'>
               <p className='text-xl font-normal tracking-tight text-primary dark:text-white lg:text-8xl'>
@@ -100,7 +101,7 @@ function App() {
             {/* Social Icons */}
           </div>
 
-{/* --------------------------------- GRID-3 Designify section starts here  -------------------------------------- */}
+          { /* --------------------------------- GRID-3 Designify section starts here  -------------------------------------- */}
           <a 
           href="#"
           className='ring-1 lg:row-span-2 flex flex-col justify-between group hover:ring-primary/10 dark:hover:ring-white/20 duration-300 h-full dark:ring-white/10 ring-primary/5 lg:row-start-2 md:grid-cols-2 md:grid lg:gap-0 md:items-center md:gap-12 lg:grid-cols-none lg:col-start-2 lg:col-span-2 rounded-3xl p-8 bg-[#1A1A1A] dark:bg-secondary shadow-xl dark:shadow-thick'>
@@ -124,11 +125,11 @@ function App() {
               className='rounded-2xl group-hover:ring-white/20 duration-300 invert dark:invert-0 aspect-[4/4] bg-primary/5 dark:bg-primary ring-1 ring-white/10  object-cover' alt="" />
             </div>
           </a>
-{/* --------------------------------- GRID-3 Designify section ends here  -------------------------------------- */}
+          {/* --------------------------------- GRID-3 Designify section ends here  -------------------------------------- */}
 
 
 
-{/* --------------------------------- GRID-3 Resume section ends here  -------------------------------------- */}
+          {/* --------------------------------- GRID-3 Resume section ends here  -------------------------------------- */}
           <div className='ring-1 dark:ring-white/10  ring-primary/5 flex flex-col justify-between items-center rounded-3xl shadow-xl dark:shadow-thick p-8  bg-[#1A1A1A] dark:bg-secondary overflow-hidden text-center lg:text-left'>
               <div> 
                 <p className='text-xl text-primary dark:text-white lg:text-7xl tracking-tight'>
@@ -151,11 +152,11 @@ function App() {
               </div>
           </div>
 
-{/* --------------------------------- GRID-3 Resume section ends here  -------------------------------------- */}
+          {/* --------------------------------- GRID-3 Resume section ends here  -------------------------------------- */}
 
 
 
-{/* --------------------------------- GRID-4 << My Projects section starts here >>  -------------------------------------- */}
+          {/* --------------------------------- GRID-4 << My Projects section starts here >>  -------------------------------------- */}
 
           <a 
           href="#"
@@ -169,11 +170,11 @@ function App() {
             </p>
           </a>
 
-{/* --------------------------------- GRID-4 << My Projects section ends here >>  -------------------------------------- */}
+          {/* --------------------------------- GRID-4 << My Projects section ends here >>  -------------------------------------- */}
 
 
 
-{/* --------------------------------- GRID-5 Skills section starts here   -------------------------------------- */}
+          {/* --------------------------------- GRID-5 Skills section starts here   -------------------------------------- */}
           <div className='lg:row-span-2 ring-1 dark:ring-white/10 ring-primary/5 hover:ring-primary/5 bg-[#1A1A1A] dark:bg-secondary dark:hover:ring-white/20 overflow-hidden duration-300 shadow-xl dark:shadow-thick rounded-3xl p-8'>
             <a href="#">
               <div className='-mr-28 pr-4 '>
@@ -231,18 +232,110 @@ function App() {
             </div>
 
           </div>
-{/* --------------------------------- GRID-5 Skills section ends here   -------------------------------------- */}
+          {/* --------------------------------- GRID-5 Skills section ends here   -------------------------------------- */}
 
 
 
-{/* --------------------------------- GRID-3 Resume section ends here  -------------------------------------- */}
-{/* --------------------------------- GRID-3 Resume section ends here  -------------------------------------- */}
-{/* --------------------------------- GRID-3 Resume section ends here  -------------------------------------- */}
-{/* --------------------------------- GRID-3 Resume section ends here  -------------------------------------- */}
-{/* --------------------------------- GRID-3 Resume section ends here  -------------------------------------- */}
+          {/* --------------------------------- GRID-3 message section starts here  -------------------------------------- */}
+
+          <div className='ring-1 dark:ring-white/10 ring-primary/5 flex flex-col p-8 h-full justify-center items-center rounded-3xl overflow-hidden relative lg:col-span-2 lg:row-start-4 bg-[#1A1A1A] dark:bg-secondary shadow-xl dark:shadow-thick'>
+            <div className='relative p-8 text-center w-full'>
+                <p className='text-xl tracking-tight font-medium text-primary dark:text-white md:text-6xl'>
+                  Subscribe <span className='lg:block'>to my newsletter</span>
+                </p>
+                <form
+                 className='mt-6 sm:flex w-full lg:max-w-sm mx-auto'>
+                    <label htmlFor="email-address" className='sr-only'>Email address</label>
+                    <input 
+                    type="email"
+                    name='email-address'
+                    id='email-address'
+                    autoComplete='email'
+                    required=''
+                    className='block w-full h-12 px-4 py-2 text-sm text-zinc-500 bg-zinc-100 dark:bg-tertiary ring-1 dark:ring-white/10 ring-primary/5 rounded-lg appearance-none focus:ring-white/20 placeholder-zinc-400 focus:border-zinc-300 focus:bg-primary focus:outline-none focus:ring-indigo-500 sm:text-sm'
+                    placeholder='Enter your email' />
+
+                    <div className='mt-4 sm:ml-2 sm:mt-0 sm:flex-shrink-0'>
+                        <button
+                        type='submit'
+                        className='text-sm py-2 w-full px-4 h-12 font-semibold focus:ring-2 rounded-lg bg-primary dark:bg-white dark:text-primary dark:hover:text-white hover:text-primary dark:hover:bg-white/5 hover:bg-primary/10 text-black flex duration-200 focus:ring-offset-2 focus:ring-inline-flex items-center justify-between'>
+                        Subscribe
+                        </button>
+                    </div>
+                </form>
+            </div>
+          </div>
+
+          {/* --------------------------------- GRID-3 Testimonials section starts here  -------------------------------------- */}
+
+          <div className='ring-1 dark:ring-white/10 ring-primary/5 shadow-xl dark:shadow-thick rounded-3xl p-8 lg:row-start-4 h-full flex flex-col justify-between bg-[#1A1A1A] dark:bg-secondary'>
+              <p 
+                className='text-xl tracking-tight font-medium text-primary dark:text-white md:text-5xl'>
+                Testimonials
+              </p> 
+              <div className='md:grid md:grid-cols-2 gap-6 lg:grid-cols-1'>
+                <div className='mt-2'>
+                  <p className='text-sm text-pink-500 dark:text-pink-400'>
+                      "Big fan of your themes. They're well-organized, look clean, and
+                      are fast."
+                  </p>
+                  <p className='text-xs mt-2 text-zinc-500'>
+                    <span className='block text-xs'>Yuvraj Bali</span>
+                    <span className='block text-xs'>Developer</span>
+                  </p>
+                </div>
+                <div className='mt-2'>
+                  <p className='text-sm text-pink-500 dark:text-pink-400'>
+                      "Big fan of your themes. They're well-organized, look clean, and
+                      are fast."
+                  </p>
+                  <p className='text-xs mt-2 text-zinc-500'>
+                    <span className='block text-xs'>Yuvraj Bali</span>
+                    <span className='block text-xs'>Developer</span>
+                  </p>
+                </div>
+                <div className='mt-2'>
+                  <p className='text-sm text-orange-500 dark:text-orange-300'>
+                      "Amazing resource using @react and @tailwindcss! I got it
+                      a week ago and have found the contents really useful.
+                  </p>
+                  <p className='text-xs mt-2 text-zinc-500'>
+                      <span className='block text-xs'>Ayush</span>
+                      <span className='block text-xs'>Entrepreneur</span>
+                  </p>    
+                </div>
+              </div> 
+          </div>
+
+
+          {/* --------------------------------- GRID-3 Testimonials section ends here  -------------------------------------- */}
+
+
+
+
+
+          {/* --------------------------------- GRID-3 last section starts here  -------------------------------------- */}
+          <div className='lg:col-span-2 xl:col-span-1 h-full flex flex-col justify-between lg:row-span-2 ring-1 dark:ring-white/10 ring-primary/5 bg-[#1A1A1A] dark:bg-secondary shadow-xl dark:shadow-thick rounded-3xl p-8'>
+              <p className='text-xl tracking-tight font-medium text-primary dark:text-white md:text-3xl'>
+                  As a digital designer, my expertise lies in crafting distinctive
+                  visual identities for digital products.
+                    <br />
+                    <br />
+                  I hold the belief that an engaging design begins with shared values,
+                  transparent communication, and a genuine respect for the audience.
+              </p>
+              <p className='text-primary dark:text-white '>Prateek Singh Chouhan</p>
+          </div>
+
+
+
+
+
+        {/* --------------------------------- GRID-3 last section Ends here  -------------------------------------- */}
+          </div>
+        </div>
 
       </section>
-    </>
   )
 }
 
