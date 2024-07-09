0562a362c6c7b2e814d003b45a834f8a12644bf5
Prateeksingchn <prateeksinghchouhan4499@gmail.com>
2024-07-09 13:38:04 +0530

    Complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -66,19 +66,19 @@ function App() {
           {/* Social Media Section */}
           <div className='ring-1 dark:ring-white/10 ring-primary/5 rounded-3xl justify-between shadow-xl dark:shadow-thick items-center flex flex-col p-8 h-full bg-[#1A1A1A] dark:bg-secondary'>
             <div className='w-full'>
-              <p className='text-xl font-normal tracking-tight text-primary dark:text-white lg:text-8xl'>
+              <p className='text-6xl font-normal tracking-tight text-primary dark:text-white lg:text-8xl'>
                 Find <br /> me on
               </p>
             </div>
             <div className='grid mt-32 grid-cols-4 md:grid-cols-8 lg:grid-cols-4 gap-y-4 gap-4 justify-center items-center w-full'>
               <SocialIcon href="mailto:email@prateeksinghchouhan007.com" icon={<IoMdMail className='icon icon-tabler h-6 w-6 group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50' />} />
-              <SocialIcon href="https://github.com/Prateeksingchn" icon={<i className="icon icon-tabler h-6 w-6 ml-2 mt-2 group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-github"></i>} />
-              <SocialIcon href="https://www.linkedin.com/in/prateek-singh-chouhan-7a511b1b7" icon={<i className="icon icon-tabler h-6 w-6 ml-2 mt-2 group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-linkedin"></i>} />
-              <SocialIcon href="https://www.instagram.com/prateeksinghchouhan007" icon={<i className="icon icon-tabler h-6 w-6 ml-2 mt-2 group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-instagram"></i>} />
-              <SocialIcon href="https://twitter.com/prateeksinghchouhan" icon={<i className="icon icon-tabler h-6 w-6 ml-2 mt-2 group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-twitter"></i>} />
-              <SocialIcon href="https://www.behance.net/prateeksinghc1" icon={<i className="icon icon-tabler h-6 w-6 ml-2 mt-2 group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-behance"></i>} />
-              <SocialIcon href="https://dribbble.com/prateeeeek" icon={<i className="icon icon-tabler h-6 w-6 ml-2 mt-2 group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-dribbble"></i>} />
-              <SocialIcon href="https://in.pinterest.com/prateekchouhan96483/" icon={<i className="icon icon-tabler h-6 w-6 ml-2 mt-2 group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-pinterest"></i>} />
+              <SocialIcon href="https://github.com/Prateeksingchn" icon={<i className="icon icon-tabler h-6 w-6 ml-[5.5px] text-[22px] mt-[2px]  group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-github"></i>} />
+              <SocialIcon href="https://www.linkedin.com/in/prateek-singh-chouhan-7a511b1b7" icon={<i className="icon icon-tabler h-6 w-6 ml-[5.5px] text-[22px] mt-[2px]  group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-linkedin"></i>} />
+              <SocialIcon href="https://www.instagram.com/prateeksinghchouhan007" icon={<i className="icon icon-tabler h-6 w-6 ml-[5.5px] text-[22px] mt-[2px]  group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-instagram"></i>} />
+              <SocialIcon href="https://twitter.com/prateeksinghchouhan" icon={<i className="icon icon-tabler h-6 w-6 ml-[5.5px] text-[22px] mt-[2px]  group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-twitter"></i>} />
+              <SocialIcon href="https://www.behance.net/prateeksinghc1" icon={<i className="icon icon-tabler h-6 w-6 ml-[5.5px] text-[22px] mt-[2px]  group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-behance"></i>} />
+              <SocialIcon href="https://dribbble.com/prateeeeek" icon={<i className="icon icon-tabler h-6 w-6 ml-[5.5px] text-[22px] mt-[2px]  group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-dribbble"></i>} />
+              <SocialIcon href="https://in.pinterest.com/prateekchouhan96483/" icon={<i className="icon icon-tabler h-6 w-6 ml-[5.5px] text-[22px] mt-[2px]  group-hover:scale-125 duration-300  text-primary hover:text-primary dark:text-black group-hover text-primary:dark:text-white/50 fa-brands fa-pinterest"></i>} />
             </div>
           </div>
 
