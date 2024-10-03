9c7f2734fd2d2fe927b6ff4fe68d0f229351204e
Prateeksingchn <prateeksinghchouhan4499@gmail.com>
2024-10-03 12:55:39 +0530

    fix: Resolved minor bugs and issues

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import path from 'path'
+import fs from 'fs'
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    {
+      name: 'copy-redirects',
+      writeBundle() {
+        fs.copyFileSync(
+          path.resolve(__dirname, 'public/_redirects'),
+          path.resolve(__dirname, 'dist/_redirects')
+        )
+      }
+    }
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
