9c2ef16cf5590bf30ce8bcf0ef66743114862253
Prateeksingchn <prateeksinghchouhan4499@gmail.com>
2024-10-07 09:28:40 +0530

    fix: Minor tweaks and adjustments

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Cursor from './Cursor';
 
-function Layout({ children }) {
+function Layout({ isVisible, children, ...props }) { // Added isVisible and props
   const { cursorJSX, handleProjectsMouseEnter, handleProjectsMouseLeave } = Cursor();
 
   return (
-    <div className="App">
+    <div className="App" {...props}> {/* Spread props to filter out unknown props */}
       {cursorJSX}
       {React.Children.map(children, child =>
         React.cloneElement(child, { handleProjectsMouseEnter, handleProjectsMouseLeave })
