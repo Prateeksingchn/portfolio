1c895039e0fd84f0d004569fa56b7cf1f478c9e5
Prateeksingchn <prateeksinghchouhan4499@gmail.com>
2024-10-16 19:23:16 +0530

    fix: Resolved minor bugs

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -89,7 +89,7 @@ function ContactForm({ isDarkMode, formData, handleChange, handleInputFocus, han
           <p className={`text-sm md:text-lg mb-4 ${isDarkMode ? "text-gray-300" : "text-gray-700"} uppercase tracking-wide`}>
             Project budget (USD)
           </p>
-          <div className="flex flex-col md:flex-row space-x-0 md:space-x-12 space-y-4">
+          <div className="flex flex-col lg:flex-row space-x-0 lg:space-x-12 space-y-4 lg:space-y-0">
             {["< 2k", "2-5k", "5-10k", "> 10k"].map((budget) => (
               <label key={budget} className="relative inline-block cursor-pointer group">
                 <input
